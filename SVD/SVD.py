3e961b35fbbcba40b2c6226da16cfbd1f1a24750
郑启航 <597323109@qq.com>
2020-06-19 09:44:00 +0800

    SVD

--- SVD/SVD.py
+++ SVD/SVD.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+from functools import reduce
 
 
 class SVD(BaseModel):
@@ -14,49 +15,31 @@ class SVD(BaseModel):
 
   def fit(self, X):
     u, sigma, v = np.linalg.svd(X)
-    self.history = [['v^T', '\Sigma ', 'u'], [v, np.diag(sigma), u]]
+    self.history = [['E', 'v^T', '\Sigma ', 'u'],
+                    [np.eye(len(X)), v, np.diag(sigma), u]]
 
   def show_anime(self, save_path='gif/SVD.gif'):
     fig: plt.Figure = plt.figure()
     fig.set_tight_layout(True)
     ax: Axes3D = fig.add_subplot(1, 1, 1, projection='3d')
     ax.view_init(elev=25., azim=120.)
-    e = np.eye(len(X))
-    name = 'E'
-    ax.set_title('$' + name + '$')
-    for line, color in zip(e, ['g', 'orange', 'r']):
-      ax.quiver(0, 0, 0,
-                line[0] - 0, line[1] - 0, line[2] - 0,
-                length=0.1, normalize=True, color=color,
-                arrow_length_ratio=0.1)
 
     def update(i):
       prefixs, mats = self.history[0][:i + 1], self.history[1][:i + 1]
       prefixs.reverse()
-      fname = ''.join(prefixs) + name
-      for mat in mats:
-        fe = mat @ e
+      fname = ''.join(prefixs)
+      fe = reduce(lambda a, b: b @ a, mats)
       ax.cla()
       ax.set_title('$' + fname + '$')
       for line, color in zip(fe, ['g', 'orange', 'r']):
         ax.quiver(0, 0, 0,
                   line[0] - 0, line[1] - 0, line[2] - 0,
-                  length=0.1, normalize=True, color=color,
+                  length=0.1, normalize=False, color=color,
                   arrow_length_ratio=0.1)
-
-    def init_update():
-      fe = e
-      fname = name
-      ax.cla()
-      ax.set_title('$' + fname + '$')
-      for line, color in zip(fe, ['g', 'orange', 'r']):
-        ax.quiver(0, 0, 0,
-                  line[0] - 0, line[1] - 0, line[2] - 0,
-                  length=0.1, normalize=True, color=color,
-                  arrow_length_ratio=0.1)
-
-    anim = FuncAnimation(fig, update, init_func=init_update,
-                         frames=len(self.history[0]), interval=1000)
+      ax.set_xticks([-.1, 0, .1])
+      ax.set_yticks([-.1, 0, .1])
+      ax.set_zticks([-.1, 0, .1])
+    anim = FuncAnimation(fig, update, frames=len(self.history[0]), interval=1000)
     anim.save(save_path, writer='imagemagick', fps=1)
     plt.show()
 
